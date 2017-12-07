96d3ce8f28170f33f240f4e991bbe1938024288d
mycamach <manoli.camacho.a@gmail.com>
2017-12-06 22:27:19 -0300

    purchases ready

---
+++ db/migrate/20171207005238_add_sold_to_product.rb
@@ -0,0 +1,5 @@
+class AddSoldToProduct < ActiveRecord::Migration[5.1]
+  def change
+    add_column :products, :sold, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206224445) do
+ActiveRecord::Schema.define(version: 20171207005238) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "image"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.boolean "sold", default: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_purchases_on_product_id"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
